101d9397b15117e7d62d0c25603355554e72d599
Igor Bueno <ig.dsbueno@gmail.com>
2024-02-06 23:10:40 -0300

    Welcome to java! primeiro codigin

---
+++ JavaExec/WelcomeToJava.java
@@ -0,0 +1,9 @@
+package JavaExec;
+// import java.io.*; para funcionar apenas no hackrank
+// import java.util.*; para funcionar apenas no hackrank
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
